5efaea3eed38688850148e950ddb0ed1e77b0988
Hugo Josefson <hugo@josefson.org>
2010-10-19 08:56:23 +0200

    Generated stub project from maven-archetype-profiles.

---
+++ profiles/src/main/java/org/josefson/hugo/maven/releasetest/App.java
@@ -0,0 +1,13 @@
+package org.josefson.hugo.maven.releasetest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
